25cc7212fbe066cd582e194daf9e3e33934f2453
Moses Makangila <mosesfuego@gmail.com>
2026-02-20 23:13:47 -0800

    Psuedo code added to .py files

---
+++ src/reporting/debrief_template.py
@@ -0,0 +1,14 @@
+class DebriefGenerator:
+    def __init__(self, config):
+        pass
+
+    def generate(self, analyzed_builds):
+        """
+        Returns:
+            formatted string debrief
+        """
+        pass
+
+    def output(self, debrief):
+        # print to console for MVP
+        pass
